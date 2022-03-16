7a95be3a16d409eab7ab7e43ea6b900273b98d6e
Kshitiz Thapa <kstha022@kathmandu>
2022-03-16 18:09:07 +0545

    php_oop

--- delete.php
+++ delete.php
@@ -1,6 +1,9 @@
 <?php require_once('./config/operation.php');
  $obj= new operation();
  $id= $_GET['id'];
- $obj->delete_record($id);
+if($obj->delete_record($id)){
+$this->set_message("<div class='alert alert-danger'>your record has been deleted</div>");
+header('location:view.php');
+} 
 
 ?>
